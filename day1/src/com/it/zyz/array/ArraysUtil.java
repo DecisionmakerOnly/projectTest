437c69856d16bfd2f5d21e2ebdc39189f8d6638b
ljh <ljh96402@163.com>
2019-12-26 14:40:15 +0800

    Arrays工具类的使用

--- day1/src/com/it/zyz/array/ArraysUtil.java
+++ day1/src/com/it/zyz/array/ArraysUtil.java
@@ -14,8 +14,16 @@ public class ArraysUtil {
         boolean isEquals= Arrays.equals(array1,array2);
         System.out.println(isEquals);
         //2、String toString(int[] a)
+        System.out.println(Arrays.toString(array1));
         //3、volid fill(int [] a ,int val)
+        Arrays.fill(array1,10);
+        System.out.println(Arrays.toString(array1));
         //4、void sort(int  [] a)
+        Arrays.sort(array2);
+        System.out.println(Arrays.toString(array2));
         //5、int binarySearch(int [] a,int key)
+        int[] array3=new int[]{11,23,54,66,78,93,222,334,777,987};
+        int index=Arrays.binarySearch(array3,222);
+        System.out.println(index);
     }
 }
