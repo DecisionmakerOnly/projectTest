f512d3f9e7933b27fab146b1b0914332cf87ba15
ljh <ljh96402@163.com>
2019-12-31 16:25:48 +0800

    2019-12-31 16:25:40

--- day1/src/com/it/zyz/oop/OverLoadTest.java
+++ day1/src/com/it/zyz/oop/OverLoadTest.java
@@ -6,6 +6,18 @@ package com.it.zyz.oop;
  */
 public class OverLoadTest {
     public static void main(String[] args) {
+        OverLoadTest ot=new OverLoadTest();
+        ot.show(2);
+        ot.show("ss","ssd");
+        ot.show();
+    }
+    public void show(int i){
 
+    }
+//    public void show(String i){
+//        System.out.println("show(String)");
+//    }
+    public void show(String ... i){
+        System.out.println("show(String... str)");
     }
 }
---
+++ day1/src/com/it/zyz/oop/ValueTransferTest.java
@@ -0,0 +1,41 @@
+package com.it.zyz.oop;
+
+
+/**
+ * @author ljh
+ * @create 2019-12-31 13:58
+ */
+public class ValueTransferTest {
+    /**
+     * 关于变量赋值
+     */
+    public static void main(String[] args) {
+        System.out.println("==========基本数据类型===========");
+        int m=10;
+        int n=20;
+        System.out.println("m="+m+"n="+n);
+//        int temp=m;
+//        m=n;
+//        m=temp;
+        ValueTransferTest vtf=new ValueTransferTest();
+        vtf.swap(m,n);
+        System.out.println("m="+m+"n="+n);
+        System.out.println("===========引用数据类型===========");
+        Order o1=new Order();
+        o1.orderId=1001;
+        Order o2=o1;//赋值以后o1和o2的地址值相同，都指向了堆空间中同一个对象实体。
+
+        System.out.println("o1.orderId="+o1.orderId+"o2.orderId="+o2.orderId);
+        o2.orderId=1002;
+        System.out.println("o1.orderId="+o1.orderId+"o2.orderId="+o2.orderId);
+    }
+    public void swap(int m,int n){
+        int temp=m;
+        m=n;
+        m=temp;
+    }
+
+}
+class Order{
+    int orderId;
+}
